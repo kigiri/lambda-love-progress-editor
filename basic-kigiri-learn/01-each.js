e704f61cc2cd67aeb7533b5874f985b45aa71a9a
Clément <cdenis@student.42.fr>
2016-06-05 20:11:19 +0200

    Saving progress of basic-kigiri-learn 01-each.js - explain a bit more

---
+++ basic-kigiri-learn/01-each.js
@@ -0,0 +1,12 @@
+// CODE HERE
+// to help you, here the function signature I want :
+
+// fn is the give function
+// collection is the array for the first test
+const each = (fn, collection) => {
+  // ???
+}
+
+// if you are not familiar with ES6 arrow functions
+// watch https://www.youtube.com/watch?v=6sQDTgOqh-I
+// I strongly recommand all of this guy video, good stuff !

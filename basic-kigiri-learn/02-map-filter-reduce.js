854961ec572b75f28a7068f78e6e9bf4507b1e0c
Clément <cdenis@student.42.fr>
2016-06-06 01:45:22 +0200

    Saving progress of basic-kigiri-learn 02-map-filter-reduce.js - autosave

---
+++ basic-kigiri-learn/02-map-filter-reduce.js
@@ -0,0 +1,16 @@
+// CODE HERE
+// to help you, here the function signature I want :
+
+// fn is the give function
+// collection is the array for the first test
+const each = (fn, collection) => {
+  // ???
+  var i = -1
+  const max = collection.length
+  
+  while (++i < max) { break }
+}
+
+// if you are not familiar with ES6 arrow functions
+// watch https://www.youtube.com/watch?v=6sQDTgOqh-I
+// I strongly recommand all of this guy video, good stuff !
